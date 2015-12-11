7b76c0292307ad491b8b06ad791af9c79034c56a
Jiri Stransky <jistr@redhat.com>
2015-12-11 16:14:20 +0100

    Remove instack-only-uc setup
    I don't think anyone used it lately and have no idea if it still works,
    removing unless someone asks for it to return.

--- instack-only-uc-vm-destroy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo virsh destroy instack-only-uc
-sudo virsh undefine instack-only-uc --remove-all-storage
--- instack-only-uc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -i hosts.instack-only-uc \
-    --extra-vars '@answers.yml' \
-    "$@" \
-    playbooks/instack_only_uc.yml
